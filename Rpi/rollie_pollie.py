015be541fe81bd255552ceda144613f42da6cf56
unsatisfiedpopcorn <jefferykwoh@gmail.com>
2018-12-28 12:35:58 +0800

    Add sanity GPIO.cleanup()

--- Rpi/rollie_pollie.py
+++ Rpi/rollie_pollie.py
@@ -41,7 +41,7 @@ class RolliePollie:
 
     def tare_callback(self, channel):
         self._scale.zero(times=10)
-        print("Tared")
+        print("Tared via trigger on channel {}".format(channel))
 
     def register_callback(self, channel):
         wheelchair_weight = self._scale.get_weight_mean(NUMBER_OF_READINGS)
@@ -65,6 +65,7 @@ class RolliePollie:
         """
         :rtype: void
         """
+        GPIO.cleanup()
         GPIO.setmode(GPIO.BCM)
 
         # Falling edge triggers interrupt #
