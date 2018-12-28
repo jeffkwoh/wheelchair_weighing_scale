d12333b4402beb6f406310c5bfa23a3319e95387
unsatisfiedpopcorn <jefferykwoh@gmail.com>
2018-12-28 14:35:15 +0800

    Handle Unicode Decode Error

--- Rpi/lib/arduino_nfc.py
+++ Rpi/lib/arduino_nfc.py
@@ -62,8 +62,12 @@ class SerialNfc:
         if byte_string is None or not isinstance(byte_string, bytes):
             return None
 
-        # \x02, start of line control character is replaced
-        string_arr = byte_string.decode("utf-8").replace("\x02", "").split()  # May have decode errors
+        try:
+            # \x02, start of line control character is replaced
+            string_arr = byte_string.decode("utf-8").replace("\x02", "").split()  # May have decode errors
+        except UnicodeDecodeError:
+            print("Unicode Decode Error")
+            return None
 
         print(string_arr)
         # There should only be one wheelchair_weight (TODO: needs assertion)
